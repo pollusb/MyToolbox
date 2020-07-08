0e85f5a78aef4c0ea368047d8627128a2aed79a6
Pollus Brodeur <pollusb@gmail.com>
2020-07-08 00:36:26 -0400

    abc

--- Functions/Windows/Hostname.ps1
+++ Functions/Windows/Hostname.ps1
@@ -1,4 +1,4 @@
 Function Hostname {
     # Replacement to hostname.exe pour retourner le FQDN
-    return Invoke-Expression "(Get-WmiObject win32_computersystem).DNSHostName.ToUpper()+'.'+(Get-WmiObject win32_computersystem).Domain" # Version WMI
+    Invoke-Expression "(Get-WmiObject win32_computersystem).DNSHostName.ToUpper()+'.'+(Get-WmiObject win32_computersystem).Domain.ToLower()" # Version WMI
 }
